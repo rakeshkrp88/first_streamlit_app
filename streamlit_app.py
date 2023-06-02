fae762e5983874996ebee65b5922b55b5649aada
rakeshkrp88 <135257774+rakeshkrp88@users.noreply.github.com>
2023-06-02 21:28:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ import requests
 
 # write your own comment -what does the next line do? 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
